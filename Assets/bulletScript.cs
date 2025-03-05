eb6a3f19a88ad160330bb8b27526dfd361cbbdc3
carlosnuno1 <150867782+carlosnuno1@users.noreply.github.com>
2025-03-04 19:39:00 -0800

    basic movement and shooting implemented

---
+++ Assets/bulletScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private Camera mainCam;
+    private Rigidbody2D rb;
+    public float force;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        rb = GetComponent<Rigidbody2D>();
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+        rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * force;
+        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
