56dcc54628d90b4f93041be30fba809873cf181f
6YuQing6 <shan66@ucsc.edu>
2025-03-15 00:49:30 -0700

    fixed dodge

--- Assets/Scripts/DodgeAwayFromPlayerAction.cs
+++ Assets/Scripts/DodgeAwayFromPlayerAction.cs
@@ -1,45 +1,75 @@
 using System;
 using Unity.Behavior;
+using Unity.Properties;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
-using Unity.Properties;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "Dodge Away From Player", story: "[Boss] Dodge Away From [Player]", category: "Action", id: "493e349ca71ce4cbcdfbd85b7c93f8b4")]
+[NodeDescription(
+    name: "Dodge Away From Player",
+    story: "[Boss] Dodge Away From [Player]",
+    category: "Action",
+    id: "493e349ca71ce4cbcdfbd85b7c93f8b4"
+)]
 public partial class DodgeAwayFromPlayerAction : Action
 {
-    [SerializeReference] public BlackboardVariable<BossEnemy> Boss;
-    [SerializeReference] public BlackboardVariable<GameObject> Player;
+    [SerializeReference]
+    public BlackboardVariable<BossEnemy> Boss;
+
+    [SerializeReference]
+    public BlackboardVariable<GameObject> Player;
+
     protected override Status OnStart()
     {
         if (Boss == null)
         {
-
             Debug.LogError("BossEnemy component is missing on the Boss GameObject.");
             return Status.Failure;
         }
 
-        if (!Boss.Value.CanDodge()){
+        if (!Boss.Value.CanDodge())
+        {
             return Status.Failure;
         }
+        GameObject bullet = GameObject.FindGameObjectWithTag("Bullet");
+        Debug.Log("Bullet: " + bullet);
+        Vector2 dodgeDirection;
+        if (bullet != null)
+        {
+            Vector2 bulletDirection = bullet.GetComponent<Rigidbody2D>().linearVelocity.normalized;
+            Vector2 bossToBullet = (
+                bullet.transform.position - Boss.Value.transform.position
+            ).normalized;
+            float dotProduct = Vector2.Dot(bulletDirection, bossToBullet);
 
-        Vector2 dodgeDirection = (Boss.Value.transform.position - Player.Value.transform.position).normalized;
+            if (dotProduct > 0)
+            {
+                dodgeDirection = new Vector2(bulletDirection.y, -bulletDirection.x);
+            }
+            else
+            {
+                dodgeDirection = new Vector2(-bulletDirection.y, bulletDirection.x);
+            }
+        }
+        else
+        {
+            dodgeDirection = (
+                Boss.Value.transform.position - Player.Value.transform.position
+            ).normalized;
+        }
         Boss.Value.Dodge(dodgeDirection);
 
         return Status.Running;
-
     }
 
     protected override Status OnUpdate()
     {
-        if (!Boss.Value.DodgeStatus()) { // if not dodging anymore return success
+        if (!Boss.Value.DodgeStatus())
+        { // if not dodging anymore return success
             return Status.Success;
         }
         return Status.Running;
     }
 
-    protected override void OnEnd()
-    {
-    }
+    protected override void OnEnd() { }
 }
-
--- Assets/Scripts/MetricsManager.cs
+++ Assets/Scripts/MetricsManager.cs
@@ -1,9 +1,9 @@
-using UnityEngine;
 using Unity.Behavior;
 using Unity.VisualScripting;
+using UnityEngine;
 
-
-public class Metrics {
+public class Metrics
+{
     public int Shots { get; private set; }
     public int SuccessfulShots { get; private set; }
     public int Dodges { get; private set; }
@@ -11,29 +11,64 @@ public class Metrics {
     public int Blocks { get; private set; }
     public int SuccessfulBlocks { get; private set; }
 
-    public int Health {get; private set;}
+    public int Health { get; private set; }
 
     private string _name;
 
-    public Metrics(string name) {
+    public Metrics(string name)
+    {
         _name = name;
     }
 
-    private void UpdateBlackBoard(){
+    private void UpdateBlackBoard()
+    {
         MetricsManager.instance.UpdateBlackBoard();
     }
 
+    public void RecordShot()
+    {
+        Shots++;
+        UpdateBlackBoard();
+    }
 
-    public void RecordShot() { Shots++; UpdateBlackBoard();}
-    public void RecordSuccessfulShot() { SuccessfulShots++; UpdateBlackBoard();}
-    public void RecordDodge() { Dodges++; UpdateBlackBoard();}
-    public void RecordSuccessfulDodge() { SuccessfulDodges++; UpdateBlackBoard(); }
-    public void RecordBlock() { Blocks++; UpdateBlackBoard();}
-    public void RecordSuccessfulBlock() { SuccessfulBlocks++; UpdateBlackBoard(); }
+    public void RecordSuccessfulShot()
+    {
+        SuccessfulShots++;
+        UpdateBlackBoard();
+    }
 
-    public void RecordHealth(int health) { Health = health; UpdateBlackBoard(); }
+    public void RecordDodge()
+    {
+        Dodges++;
+        UpdateBlackBoard();
+    }
 
-    public void PrintStats() {
+    public void RecordSuccessfulDodge()
+    {
+        SuccessfulDodges++;
+        UpdateBlackBoard();
+    }
+
+    public void RecordBlock()
+    {
+        Blocks++;
+        UpdateBlackBoard();
+    }
+
+    public void RecordSuccessfulBlock()
+    {
+        SuccessfulBlocks++;
+        UpdateBlackBoard();
+    }
+
+    public void RecordHealth(int health)
+    {
+        Health = health;
+        UpdateBlackBoard();
+    }
+
+    public void PrintStats()
+    {
         // Debug.Log($"{_name} \n Shots: {SuccessfulShots}/{Shots}, Dodges: {SuccessfulDodges}/{Dodges}, Blocks: {SuccessfulBlocks}/{Blocks}");
     }
 }
@@ -51,33 +86,39 @@ public class MetricsManager : MonoBehaviour
 
     private float totalDistance = 0;
     private int distanceSamples = 0;
-    
+
     public float distanceInvokeTime = 1.0f;
     public float CurrentDistance { get; private set; }
-    public float AverageDistance  { get; private set; }
+    public float AverageDistance { get; private set; }
 
-    public static MetricsManager instance {get; private set;}
+    public static MetricsManager instance { get; private set; }
 
-    [SerializeField] private BehaviorGraphAgent behaviorGraphAgent;
+    [SerializeField]
+    private BehaviorGraphAgent behaviorGraphAgent;
 
     private void Awake()
     {
-        if (instance != null && instance != this) {
+        if (instance != null && instance != this)
+        {
             Destroy(this);
-        } else {
+        }
+        else
+        {
             instance = this;
         }
         behaviorGraphAgent = boss.GetComponent<BehaviorGraphAgent>();
         Debug.Log(behaviorGraphAgent);
-    } 
+    }
 
     public void Start()
     {
         InvokeRepeating(nameof(RecordDistance), distanceInvokeTime, distanceInvokeTime);
     }
 
-    private void RecordDistance() {
-        if (boss && player) {
+    private void RecordDistance()
+    {
+        if (boss && player)
+        {
             CurrentDistance = Vector3.Distance(player.transform.position, boss.transform.position);
             totalDistance += CurrentDistance;
             distanceSamples++;
@@ -86,11 +127,13 @@ public class MetricsManager : MonoBehaviour
             UpdateBlackBoard();
         }
     }
-    
+
     // https://docs.unity3d.com/Packages/com.unity.behavior@1.0/manual/blackboard-variables.html
     // https://docs.unity3d.com/Packages/com.unity.behavior@1.0/api/Unity.Behavior.BlackboardReference.html
-    public void UpdateBlackBoard(){
-        if (behaviorGraphAgent == null) return;
+    public void UpdateBlackBoard()
+    {
+        if (behaviorGraphAgent == null)
+            return;
 
         var blackboard = behaviorGraphAgent.BlackboardReference;
 
@@ -98,38 +141,56 @@ public class MetricsManager : MonoBehaviour
         {
             if (!blackboard.SetVariableValue(key, value))
             {
-
                 blackboard.AddVariable(key, value);
             }
         }
 
         SetOrAddVariable("PlayerShots", playerMetrics.Shots);
         SetOrAddVariable("PlayerSuccessfulShots", playerMetrics.SuccessfulShots);
-        SetOrAddVariable("PlayerShotAccuracy", playerMetrics.Shots > 0 ? (float)playerMetrics.SuccessfulShots / playerMetrics.Shots : 0);
+        SetOrAddVariable(
+            "PlayerShotAccuracy",
+            playerMetrics.Shots > 0 ? (float)playerMetrics.SuccessfulShots / playerMetrics.Shots : 0
+        );
         SetOrAddVariable("PlayerDodges", playerMetrics.Dodges);
         SetOrAddVariable("PlayerSuccessfulDodges", playerMetrics.SuccessfulDodges);
-        SetOrAddVariable("PlayerDodgeAccuracy", playerMetrics.Dodges > 0 ? (float)playerMetrics.SuccessfulDodges / playerMetrics.Dodges : 0);
+        SetOrAddVariable(
+            "PlayerDodgeAccuracy",
+            playerMetrics.Dodges > 0
+                ? (float)playerMetrics.SuccessfulDodges / playerMetrics.Dodges
+                : 0
+        );
         SetOrAddVariable("PlayerBlocks", playerMetrics.Blocks);
         SetOrAddVariable("PlayerSuccessfulBlocks", playerMetrics.SuccessfulBlocks);
-        SetOrAddVariable("PlayerBlockAccuracy", playerMetrics.Blocks > 0 ? (float)playerMetrics.SuccessfulBlocks / playerMetrics.Blocks : 0);
-        
+        SetOrAddVariable(
+            "PlayerBlockAccuracy",
+            playerMetrics.Blocks > 0
+                ? (float)playerMetrics.SuccessfulBlocks / playerMetrics.Blocks
+                : 0
+        );
+
         SetOrAddVariable("BossShots", bossMetrics.Shots);
         SetOrAddVariable("BossSuccessfulShots", bossMetrics.SuccessfulShots);
-        SetOrAddVariable("BossShotAccuracy", bossMetrics.Shots > 0 ? (float)bossMetrics.SuccessfulShots / bossMetrics.Shots : 0);
+        SetOrAddVariable(
+            "BossShotAccuracy",
+            bossMetrics.Shots > 0 ? (float)bossMetrics.SuccessfulShots / bossMetrics.Shots : 0
+        );
         SetOrAddVariable("BossDodges", bossMetrics.Dodges);
         SetOrAddVariable("BossSuccessfulDodges", bossMetrics.SuccessfulDodges);
-        SetOrAddVariable("BossDodgeAccuracy", bossMetrics.Dodges > 0 ? (float)bossMetrics.SuccessfulDodges / bossMetrics.Dodges : 0);
+        SetOrAddVariable(
+            "BossDodgeAccuracy",
+            bossMetrics.Dodges > 0 ? (float)bossMetrics.SuccessfulDodges / bossMetrics.Dodges : 0
+        );
         SetOrAddVariable("BossBlocks", bossMetrics.Blocks);
         SetOrAddVariable("BossSuccessfulBlocks", bossMetrics.SuccessfulBlocks);
-        SetOrAddVariable("BossBlockAccuracy", bossMetrics.Blocks > 0 ? (float)bossMetrics.SuccessfulBlocks / bossMetrics.Blocks : 0);
+        SetOrAddVariable(
+            "BossBlockAccuracy",
+            bossMetrics.Blocks > 0 ? (float)bossMetrics.SuccessfulBlocks / bossMetrics.Blocks : 0
+        );
 
         SetOrAddVariable("CurrentDistance", CurrentDistance);
         SetOrAddVariable("AverageDistance", AverageDistance);
 
         SetOrAddVariable("PlayerHealth", playerMetrics.Health);
         SetOrAddVariable("BossHealth", bossMetrics.Health);
-
     }
-
-
 }
